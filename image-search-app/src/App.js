65715f0116fbac6530114949629bd122a401c212
Sreehari <sreeharins001@gmail.com>
2023-12-18 13:39:09 +0530

    minor styling

--- image-search-app/src/App.js
+++ image-search-app/src/App.js
@@ -26,9 +26,16 @@ function App() {
            width:'100%',
            justifyContent:'center',
            alignItems:'center',
-           marginTop:'25px'}}>
+           marginTop:'25px',
+           }}>
  <img
-              style={{ display: 'block', height: '300px', width: '300px' }}
+              style={{ 
+                display: 'block', 
+                height: '300px', 
+                width: '300px',
+                borderTopRightRadius:'5px',
+                borderTopLeftRadius:'5px',
+                boxShadow:'0 2px 5px rgba(0,0,0,0.5)'  }}
               alt={image.id}
               src={image.url}></img>
 
@@ -38,7 +45,8 @@ onClick={handleClick}
               style={{
                 width:'300px',
                 borderTopRightRadius:'0px',
-                borderTopLeftRadius:'0px'
+                borderTopLeftRadius:'0px',
+                boxShadow:'0 2px 5px rgba(0,0,0,0.5)'
               }}>Click to see a Cat</button>
              
 
